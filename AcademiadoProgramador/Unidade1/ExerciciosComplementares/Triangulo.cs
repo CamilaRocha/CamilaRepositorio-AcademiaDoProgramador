464288769ae9d9cfc9b1aec02eca4994ea09a4a7
caa96 <camila_caah96@hotmail.com>
2014-07-30 11:28:02 -0300

    Atualizando

---
+++ AcademiadoProgramador/Unidade1/ExerciciosComplementares/Triangulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidade1.ExerciciosComplementares
+{
+    class Triangulo
+    {
+        /* 1) Crie umprograma que imprima na tela um triângulo de “*”.*/
+        public static void Main1(string[] args)
+        {
+            Console.WriteLine("     *        ");
+            Console.WriteLine("    ***       ");
+            Console.WriteLine("   *****      ");
+            Console.WriteLine("  *******     ");
+            Console.WriteLine(" *********    ");
+            Console.WriteLine("***********   ");
+            Console.ReadLine();
+        }
+    }
+}
